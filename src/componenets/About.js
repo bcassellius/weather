f0dcc3acbd8aa48906561026d290a0fd115b917d
Bri <bhilliker@gmail.com>
2022-04-13 09:01:13 -0500

    creating components

---
+++ src/componenets/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = ()=>{
+    return(
+        <div>
+            <h1>About</h1>
+            <p>
+                I am testing out this app to see if I can create a weather forcast page.
+            </p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/componenets/Pet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pet = ()=>{
+    return(
+        <div>
+            <h1>Ginger</h1>
+            <p>breed: Brittany Spaniel</p>
+        </div>
+    )
+}
+
+export default Pet;
